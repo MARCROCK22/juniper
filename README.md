34abb5fff4764490b15ee19eb656a882d54e19e1
VyrekXD <vyrekxd@gmail.com>
2022-03-30 16:28:55 -0600

    si

---
+++ README.md
@@ -0,0 +1,11 @@
+# juniper
+
+![](./src/images/sinduda.png)
+
+Juniper plushies
+
+# Uso
+
+https://juniper-plushes.herokuapp.com/get/<nombre>
+
+https://juniper-plushes.herokuapp.com/get/sinduda
--- src/index.js
+++ src/index.js
@@ -1,40 +1,47 @@
-import fs from 'fs';
-import path from 'path';
-import express from 'express';
-const app = express();
-const PORT = process.env.PORT || 3000;
-const __dirname = path.join(process.cwd(), 'src');
-const files = fs.readdirSync(path.join(__dirname, 'images'));
-import { fileTypeFromBuffer } from 'file-type';
+import fs from 'fs'
+import path from 'path'
+import express from 'express'
+import { fileTypeFromBuffer } from 'file-type'
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+const __dirname = path.join(process.cwd(), 'src')
+const files = fs.readdirSync(path.join(__dirname, 'images'))
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+	console.log(`Server running on port ${PORT}`)
+})
 
 app.get('/get/:name', async (req, res) => {
-    const imageFinded = files.find(file =>
-        (file.split('.')[0] === req.params.name)
-        ||
-        (file === req.params.name)
-        ||
-        (file == req.params.name.split('.')[0])
-    ) || '404.png';
-    const file = await fs.promises.readFile(path.join(__dirname, 'images', imageFinded))
-    const m = await fileTypeFromBuffer(file).then(i => i.mime);
-    res.writeHead(200, {
-        "Content-Type": m,
-        "Content-Length": file.length
-    });
-    res.status(200).end(file);
-});
+	const imageFinded =
+		files.find(
+			(file) =>
+				file.split('.')[0] === req.params.name ||
+				file === req.params.name ||
+				file == req.params.name.split('.')[0]
+		) || '404.png'
+	const file = await fs.promises.readFile(
+		path.join(__dirname, 'images', imageFinded)
+	)
+	const m = await fileTypeFromBuffer(file).then((i) => i.mime)
+	res.writeHead(200, {
+		'Content-Type': m,
+		'Content-Length': file.length
+	})
+	res.status(200).end(file)
+})
 
 app.get('/random', async (req, res) => {
-    const imageFinded = files[Math.floor(Math.random() * files.length)];
-    const file = await fs.promises.readFile(path.join(__dirname, 'images', imageFinded))
-    const m = await fileTypeFromBuffer(file).then(i => i.mime);
-    res.writeHead(200, {
-        "Content-Type": m,
-        "Content-Length": file.length
-    });
-    res.end(file);
-});
+	const imageFinded = files[Math.floor(Math.random() * files.length)]
+	const file = await fs.promises.readFile(
+		path.join(__dirname, 'images', imageFinded)
+	)
+	const m = await fileTypeFromBuffer(file).then((i) => i.mime)
+	res.writeHead(200, {
+		'Content-Type': m,
+		'Content-Length': file.length
+	})
+	res.end(file)
+})
